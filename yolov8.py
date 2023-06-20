38376f3581a2c4a4a1fc2bd89d375aab7995c882
Mor Schenavsky <m.schenavsky@gmail.com>
2023-06-20 22:46:53 +0300

    created flags

--- yolov8.py
+++ yolov8.py
@@ -36,194 +36,199 @@ torch.manual_seed(0)
 # list with datasets names
 dataset_names = ["coco128", "mouse", "zebra", "windows", "kangaroos"]
 
-#iou_list = ['coco_iou', 'mouse_iou', 'zebra_iou']
-
 # create dictionary to save iou results for all datasets
 iou_dict = {}
 
+# folder name to save dataframes
+FOLDER_NAME = 'dataframes'
+
+# Flags
+MODEL_FLAG = 'No'
 PREDICTION_FLAG = 'No'
+SAVE_FLAG = 'No'
 
-"ADD FLAG"
+" Predict images and create dataframe if PREDICTION_FLAG is Yes"
 
-if PREDICTION_FLAG=='Yes':
+if MODEL_FLAG == 'Yes':
 
     ''' YOLOv8 model '''
 
     ''' 1 Create YOLOv8 model '''
     locale.getpreferredencoding = lambda: "UTF-8"
 
+    # Load a model
+    model = YOLO('yolov8n.yaml')  # build a new model from YAML
+    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
-# Load a model
-# model = YOLO('yolov8n.yaml')  # build a new model from YAML
-# model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-# model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+    # Train the model
+    model.train(data='coco128.yaml', epochs=10, imgsz=640)
 
-# Train the model
-# model.train(data='coco128.yaml', epochs=10, imgsz=640)
+    ''' 2 Save the trained model '''
+    # export the model
+    model.export()
 
-''' 2 Save the trained model '''
-# export the model
-# model.export()  
+if PREDICTION_FLAG == 'Yes':
 
-''' 3 Load trained model & example '''
-#model_trained = YOLO(utils.repo_image_path('/best.torchscript'), task='detect')
+    ''' 3 Load trained model & example '''
+    model_trained = YOLO(utils.repo_image_path('/best.torchscript'), task='detect')
 
-# ** Example**
-#image_path = utils.repo_image_path('/Kangaroos/00050.jpg')
-#utils.predict_plot_image(image_path,model_trained)
+    """ Example """
+    #image_path = utils.repo_image_path('/Kangaroos/00050.jpg')
+    #utils.predict_plot_image(image_path,model_trained)
 
-''' Predict COCO128 Dataset '''
+    ''' Predict COCO128 Dataset '''
 
-coco128_path = utils.repo_image_path('/coco128/image')
-coco_annos_dir = utils.repo_image_path('/coco128/annotations')
+    coco128_path = utils.repo_image_path('/coco128/image')
+    coco_annos_dir = utils.repo_image_path('/coco128/annotations')
 
-#df_coco, coco_iou = utils.pipeline('coco128', coco128_path, coco_annos_dir, 'jpg', model_trained)
+    df_coco, coco_iou = utils.pipeline('coco128', coco128_path, coco_annos_dir, 'jpg', model_trained)
 
-#iou_dict["coco128"] = coco_iou
+    iou_dict["coco128"] = coco_iou
 
-''' Predict Mouse Dataset '''
+    ''' Predict Mouse Dataset '''
 
-mouse_path = utils.repo_image_path('/Mouse')
-mouse_annos_dir = utils.repo_image_path('/Mouse/annotations')
+    mouse_path = utils.repo_image_path('/Mouse')
+    mouse_annos_dir = utils.repo_image_path('/Mouse/annotations')
 
-#df_mouse, mouse_iou = utils.pipeline('mouse', mouse_path, mouse_annos_dir, 'jpg', model_trained)
+    df_mouse, mouse_iou = utils.pipeline('mouse', mouse_path, mouse_annos_dir, 'jpg', model_trained)
 
-#iou_dict["mouse"] = mouse_iou
+    iou_dict["mouse"] = mouse_iou
 
-# images with low score
-#df_mouse_low_score = df_mouse[(df_mouse["avg_score"] < 0.5)].sort_values(by=['avg_score'])
+    # images with low score
+    df_mouse_low_score = df_mouse[(df_mouse["avg_score"] < 0.5)].sort_values(by=['avg_score'])
 
-# print images with low score
-#mouse_low_score_lst = df_mouse_low_score.index.values.tolist()
-#for image in mouse_low_score_lst:
-#  utils.print_image_by_dataset_and_name(image, "Mouse", model_trained)
+    # print images with low score
+    #mouse_low_score_lst = df_mouse_low_score.index.values.tolist()
+    #for image in mouse_low_score_lst:
+    #    utils.print_image_by_dataset_and_name(image, "Mouse", model_trained)
 
-# print images with high score
-#df_mouse_high_score = df_mouse[(df_mouse["avg_score"] > 0.8)].sort_values(by=['avg_score'])
+    # images with high score
+    df_mouse_high_score = df_mouse[(df_mouse["avg_score"] > 0.8)].sort_values(by=['avg_score'])
 
-#mouse_high_score_list = df_mouse_high_score.index.values.tolist()
+    # print images with high score
+    #mouse_high_score_list = df_mouse_high_score.index.values.tolist()
+    #for image in mouse_high_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Mouse",model_trained)
 
-#for image in mouse_high_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Mouse",model_trained)
+    """ Predict Zebras Dataset """
 
-""" Predict Zebras Dataset """
+    zebra_image_path = utils.repo_image_path('/Zebra')
+    zebra_annos_dir = utils.repo_image_path('/Zebra/annotations')
 
-zebra_image_path = utils.repo_image_path('/Zebra')
-zebra_annos_dir = utils.repo_image_path('/Zebra/annotations')
+    df_zebra, zebra_iou = utils.pipeline('zebra', zebra_image_path, zebra_annos_dir, 'jpg', model_trained)
 
-#df_zebra, zebra_iou = utils.pipeline('zebra', zebra_image_path, zebra_annos_dir, 'jpg', model_trained)
+    iou_dict["zebra"] = zebra_iou
 
-#iou_dict["zebra"] = zebra_iou
+    # print low score images
+    #df_zebra_low_score = df_zebra[(df_zebra["avg_score"] < 0.5)].sort_values(by=['avg_score'])
+    #zebra_low_score_list = df_zebra_low_score.index.values.tolist()
 
-# print low score images
-#df_zebra_low_score = df_zebra[(df_zebra["avg_score"] < 0.5)].sort_values(by=['avg_score'])
-#zebra_low_score_list = df_zebra_low_score.index.values.tolist()
+    #for image in zebra_low_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Zebra",model_trained)
 
-#for image in zebra_low_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Zebra",model_trained)
+    # print high score images
+    #df_zebra_low_score = df_zebra[(df_zebra["avg_score"] > 0.8)].sort_values(by=['avg_score'])
+    #zebra_low_score_list = df_zebra_low_score.index.values.tolist()
 
-# print high score images
-#df_zebra_low_score = df_zebra[(df_zebra["avg_score"] > 0.8)].sort_values(by=['avg_score'])
-#zebra_low_score_list = df_zebra_low_score.index.values.tolist()
+    #for image in zebra_low_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Zebra",model_trained)
 
-#for image in zebra_low_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Zebra",model_trained)
+    """ Predict Windows Dataset """
 
-""" Predict Windows Dataset """
+    windows_image_path = utils.repo_image_path('/Street windows')
+    windows_annos_dir = utils.repo_image_path('/Street windows/annotations')
 
-windows_image_path = utils.repo_image_path('/Street windows')
-windows_annos_dir = utils.repo_image_path('/Street windows/annotations')
+    df_windows, windows_iou = utils.pipeline('windows', windows_image_path, windows_annos_dir, 'jpg', model_trained, '.xml', None)
 
-#df_windows, windows_iou = utils.pipeline('windows', windows_image_path, windows_annos_dir, 'jpg', model_trained, '.xml', None)
-#print(windows_iou)
+    iou_dict["windows"] = windows_iou
 
-#iou_dict["windows"] = windows_iou
+    # print low score images
+    #df_windows_low_score = df_windows[(df_windows["avg_score"] < 0.5)].sort_values(by=['avg_score'])
+    #windows_low_score_list = df_windows_low_score.index.values.tolist()
 
-# print low score images
-#df_windows_low_score = df_windows[(df_windows["avg_score"] < 0.5)].sort_values(by=['avg_score'])
-#windows_low_score_list = df_windows_low_score.index.values.tolist()
+    #for image in windows_low_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Street windows",model_trained)
 
-#for image in windows_low_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Street windows",model_trained)
+    """ Predict Kangaroos Dataset """
 
-# try bad example
-#window_example = utils.repo_image_path('/Street windows/000003.jpg')
-#utils.predict_plot_image(window_example,model_trained)
+    kangaroos_image_path = utils.repo_image_path('/Kangaroos')
+    kangaroos_annos_dir = utils.repo_image_path('/Kangaroos/annotations')
 
-# Try good axample
-#window_example2 = utils.repo_image_path('/Street windows/000004.jpg')
-#utils.predict_plot_image(window_example2,model_trained)
+    df_kangaroos, kangaroos_iou = utils.pipeline('kangaroos', kangaroos_image_path, kangaroos_annos_dir,'jpg', model_trained, '.xml', None)
 
-""" Predict Kangaroos Dataset """
+    iou_dict["kangaroos"] = kangaroos_iou
 
-kangaroos_image_path = utils.repo_image_path('/Kangaroos')
-kangaroos_annos_dir = utils.repo_image_path('/Kangaroos/annotations')
+    # print low score images
+    #df_kangaroos_low_score = df_kangaroos[(df_kangaroos["avg_score"] < 0.5)].sort_values(by=['avg_score'])
+    #kangaroos_low_score_list = df_kangaroos_low_score.index.values.tolist()
 
-#df_kangaroos, kangaroos_iou = utils.pipeline('kangaroos', kangaroos_image_path, kangaroos_annos_dir,'jpg', model_trained, '.xml', None)
+    #for image in kangaroos_low_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Kangaroos",model_trained)
 
-#iou_dict["kangaroos"] = kangaroos_iou
+    """ Predict Face mask Dataset """
 
-# print low score images
-#df_kangaroos_low_score = df_kangaroos[(df_kangaroos["avg_score"] < 0.5)].sort_values(by=['avg_score'])
-#kangaroos_low_score_list = df_kangaroos_low_score.index.values.tolist()
+    #face_mask_image_path = utils.repo_image_path('/Face mask dataset')
+    #face_mask_annos_dir = utils.repo_image_path('/Face mask dataset/annotations')
 
-#for image in kangaroos_low_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Kangaroos",model_trained)
+    #df_face_mask, face_mask_iou = utils.pipeline('face_mask', face_mask_image_path, face_mask_annos_dir, 'jpg', model_trained, '.xml')
 
-""" Predict Face mask Dataset """
+    #iou_dict["face mask"] = face_mask_iou
 
-#face_mask_image_path = utils.repo_image_path('/Face mask dataset')
-#face_mask_annos_dir = utils.repo_image_path('/Face mask dataset/annotations')
+    # print low score images
+    #df_face_mask_low_score = df_face_mask[(df_face_mask["avg_score"] < 0.5)].sort_values(by=['avg_score'])
+    #face_mask_low_score_list = df_face_mask_low_score.index.values.tolist()
 
-#df_face_mask, face_mask_iou = utils.pipeline('face_mask', face_mask_image_path, face_mask_annos_dir, 'jpg', model_trained, '.xml')
+    #for image in face_mask_low_score_list:
+    #    utils.print_image_by_dataset_and_name(image, "Face mask dataset",model_trained)
 
-#iou_dict["face mask"] = face_mask_iou
+    """ Predict B&W Dataset """
 
-# print low score images
-#df_face_mask_low_score = df_face_mask[(df_face_mask["avg_score"] < 0.5)].sort_values(by=['avg_score'])
-#face_mask_low_score_list = df_face_mask_low_score.index.values.tolist()
+    #bw_zebra_image_path = utils.repo_image_path('/BW-Zebra')
 
-#for image in face_mask_low_score_list:
-#  utils.print_image_by_dataset_and_name(image, "Face mask dataset",model_trained)
+    # create folder for B&W dataset
+    #if not os.path.exists(bw_zebra_image_path):
+    #    os.makedirs(bw_zebra_image_path)
 
-""" Predict B&W Dataset """
+    # convert images to grayscale
+    #for filename in os.listdir(zebra_image_path):
+    #    if filename.endswith(".jpg") or filename.endswith(".png"):
+    #        img_path = os.path.join(zebra_image_path, filename)
+    #        img = Image.open(img_path).convert('L')
+    #        img.save(os.path.join(zebra_image_path, filename))
 
-#bw_zebra_image_path = utils.repo_image_path('/BW-Zebra')
+    #df_bw_zebra, bw_zebra_iou = utils.pipeline('bw_zebra', zebra_image_path, zebra_annos_dir, 'jpg', model_trained, None, "BW")
 
-# create folder for B&W dataset
-#if not os.path.exists(bw_zebra_image_path):
-#    os.makedirs(bw_zebra_image_path)
+    #iou_dict["bw_zebra"] = bw_zebra_iou
 
-# convert images to grayscale
-#for filename in os.listdir(zebra_image_path):
-#    if filename.endswith(".jpg") or filename.endswith(".png"):
-#        img_path = os.path.join(zebra_image_path, filename)
-#        img = Image.open(img_path).convert('L')
-#        img.save(os.path.join(zebra_image_path, filename))
+""" Save dataframes and IOU records if SAVE_FLAG is Yes"""
 
-#df_bw_zebra, bw_zebra_iou = utils.pipeline('bw_zebra', zebra_image_path, zebra_annos_dir, 'jpg', model_trained, None, "BW")
+if SAVE_FLAG == 'Yes':
 
-#iou_dict["bw_zebra"] = bw_zebra_iou
+    df_list = [df_coco, df_mouse, df_zebra, df_windows, df_kangaroos]
 
-""" Save dataframes and IOU records """
+    # save as CSV file
+    for (dataframe, name) in zip(df_list, dataset_names):
+        save_df.save_dataframe_as_csv(dataframe, FOLDER_NAME, name)
 
-# folder name to save dataframes
-FOLDER_NAME = 'dataframes'
+    # save IOU dict to CSV
 
-#df_list = [df_coco, df_mouse, df_zebra, df_windows, df_kangaroos]
-
-# save as CSV file
-#for (dataframe, name) in zip(df_list, dataset_names):
-#    save_df.save_dataframe_as_csv(dataframe, FOLDER_NAME, name)
-
-# save IOU dict to CSV
-
-# Convert dictionary to DataFrame
-#iou_df = pd.DataFrame(iou_dict, index=[0])
-# Save DataFrame as CSV file
-#save_df.save_dataframe_as_csv(iou_df, FOLDER_NAME, "iou_scores")
+    # Convert dictionary to DataFrame
+    iou_df = pd.DataFrame(iou_dict, index=[0])
+    # Save DataFrame as CSV file
+    save_df.save_dataframe_as_csv(iou_df, FOLDER_NAME, "iou_scores")
 
 """ Load dataframes """
+
+csv_list = ['kangaroos', 'mouse', 'windows', 'zebra', 'iou_scores']
+
+df_dict = {}
+
+for csv_file in csv_list:
+    df = pd.read_csv(utils.repo_image_path('/' + FOLDER_NAME + '/' + csv_file + '.csv')).replace('"','', regex=True)
+    df_dict[csv_file] = df
+
+"""
 import ast
 
 # Define a function to convert the string representation to a NumPy array
@@ -258,13 +263,12 @@ for csv_file in csv_list:
     #df_dict[csv_file]['relative_annotations'].to_numpy()
 
 
-
-
 #print(df_dict['mouse'])
+"""
 
 """ Image properties """
-
-'''for key, dataframe in df_dict.items():
+"""
+for key, dataframe in df_dict.items():
     if key != 'iou_scores':
         # aspect ratio
         dataframe['aspect_ratio'] = dataframe.apply(lambda row: image_utils.return_aspect_ratio(row['height'], row['width']), axis=1)
@@ -272,9 +276,8 @@ for csv_file in csv_list:
         dataframe['brightness'] = dataframe.apply(lambda row: image_utils.get_image_brightness(row['image']), axis=1)
         # image contrast
         dataframe['contrast'] = dataframe.apply(lambda row: image_utils.get_image_contrast(row['image']), axis=1)
-'''
+"""
 
-
-
+print(df_dict)
 
 """ characteristics for statistics- num_of_annotations, aspect_ratio, brightness, contrast, .... """
